42fd604a999c94001abf6d9dc544d9c757e3e136
Samuel <mmachado0222@gmail.com>
2024-08-18 15:31:49 -0300

    feat: ensure that the user can create an account and make login using refresh tokens

---
+++ src/functions/auth/signIn.ts
@@ -0,0 +1,53 @@
+import {
+	InitiateAuthCommand,
+	NotAuthorizedException,
+	UserNotConfirmedException,
+	UserNotFoundException,
+} from '@aws-sdk/client-cognito-identity-provider';
+import { cognitoClient } from '@libs/cognitoClient';
+import { bodyParser } from '@utils/bodyParser';
+import { response } from '@utils/response';
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+
+export async function handler(event: APIGatewayProxyEventV2) {
+	try {
+		const { email, password } = bodyParser(event.body);
+
+		const command = new InitiateAuthCommand({
+			ClientId: process.env.COGNITO_CLIENT_ID,
+			AuthFlow: 'USER_PASSWORD_AUTH',
+			AuthParameters: {
+				USERNAME: email,
+				PASSWORD: password,
+			},
+		});
+
+		const { AuthenticationResult } = await cognitoClient.send(command);
+
+		if (!AuthenticationResult) {
+			return response(401, { error: 'Invalid Credentials' });
+		}
+
+		const { AccessToken, RefreshToken } = AuthenticationResult;
+
+		return response(200, {
+			AccessToken,
+			RefreshToken,
+		});
+	} catch (error) {
+		if (error instanceof UserNotFoundException) {
+			return response(404, { message: 'User not found' });
+		}
+
+		if (error instanceof NotAuthorizedException) {
+			return response(401, { Error: 'Invalid Credentials' });
+		}
+
+		if (error instanceof UserNotConfirmedException) {
+			return response(401, { message: 'User not confirmed' });
+		}
+
+		console.log(error);
+		return response(500, { message: 'ERROR: Try again later /' + error });
+	}
+}
